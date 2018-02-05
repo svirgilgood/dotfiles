f0a96a181252bcbd6fc038960b6713b256075461
e2dubba <sgoodwin@atla.com>
2018-02-05 14:28:05 -0600

    delete duplicate

--- bin/notebook.py
+++ bin/notebook.py
@@ -8,20 +8,21 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('-n', '--notepath', help="Set the path to the notbooks directory")
     ap.add_argument('-d', '--description', help="Add a description")
-    ap.add_argument('term', nargs='*')
+    ap.add_argument('term', nargs='?')
     
     a = ap.parse_args()
-    term = '_'.join(a.term)
+    #term = '_'.join(a.term)
     if a.notepath:
         notepath = a.notepath
     else:
         notepath = os.environ['NOTEPATH']
 
-    if not a.term:
-        term = input('What is this project names? ')
     description = a.description
     if not a.description:
         description = "hello world"
+    term = a.term 
+    if not a.term:
+        term = input('What is the name of your project?\n')
     
     os.chdir(notepath)
     os.mkdir(term)
